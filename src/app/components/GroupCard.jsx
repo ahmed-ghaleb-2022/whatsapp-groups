c46e60b5da6eee8a81ae274afbbd561d47e2e914
ahmed-ghaleb-2022 <104622191+ahmed-ghaleb-2022@users.noreply.github.com>
2025-04-03 14:18:06 +0000

    change image tag

--- src/app/components/GroupCard.jsx
+++ src/app/components/GroupCard.jsx
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+
 import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -17,10 +17,8 @@ const GroupCard = ({ id, name, link, imageUrl }) => {
 
   return (
     <div className="flex flex-col gap-2 px-1 sm:px-4 py-4 border items-center  bg-white rounded-xl shadow-lg w-44 sm:w-64">
-      <Image
+      <img
         src={imageUrl}
-        width={150}
-        height={150}
         alt="random image"
         className="w-24 h-24 sm:w-32 sm:h-32 rounded-full shadow-2xl shadow-slate-700"
       />
